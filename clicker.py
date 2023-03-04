d2fc656b2aae44a665717a763b6b333e4c6e6e6c
lhes23 <32542078+lhes23@users.noreply.github.com>
2023-03-04 13:33:43 +0800

    change positions

--- clicker.py
+++ clicker.py
@@ -1,8 +1,7 @@
 import pyautogui as ag
 from time import sleep
 
-ag.moveTo(339,294,2,ag.easeInQuad)
-ag.click()
+# ag.moveTo(339,294,2,ag.easeInQuad)
 
 def movingUpdate(positions):
     for x,y in positions.items():
@@ -11,13 +10,19 @@ def movingUpdate(positions):
         sleep(3)
     sleep(45)
 
-    ag.moveTo(28,245,2,ag.easeInQuad)
+    ag.moveTo(19,231,2,ag.easeInQuad)
     ag.click()
     sleep(3)
 
+initial_x = 404
+initial_y = 333
+
+ag.moveTo(initial_x,initial_y,2,ag.easeInQuad)
+ag.click()
+
 while True:
     movingUpdate({
-        339:294,
-        674:324,
-        817:470,
+        initial_x:initial_y,
+        683:372,
+        871:476,
     })
