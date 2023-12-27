06e6b034b4853e72065d3fe992e72872cbeb77eb
lhes23 <32542078+lhes23@users.noreply.github.com>
2023-12-27 13:34:43 +0800

    update initial x and y

--- clicker.py
+++ clicker.py
@@ -1,6 +1,9 @@
 import pyautogui as ag
 from time import sleep
 
+initial_x = 496
+initial_y = 350
+
 def movingUpdate(positions):
     for x,y in positions.items():
         ag.moveTo(x,y,3,ag.easeInQuad)
@@ -11,9 +14,6 @@ def movingUpdate(positions):
     ag.moveTo(19,231,2,ag.easeInQuad)
     ag.click()
     sleep(3)
-
-initial_x = 404
-initial_y = 333
 
 ag.moveTo(initial_x,initial_y,2,ag.easeInQuad)
 ag.click()
